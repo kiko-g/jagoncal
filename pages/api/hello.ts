ceea08b066f7225c27e6f85f20fca7047e2b1ba3
kiko-g <kikojpgoncalves@gmail.com>
2023-08-08 10:18:52 +0100

    add: api executable c scripts template

---
+++ pages/api/hello.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { exec } from 'child_process'
+
+type Data = {
+  message: string
+}
+
+// @desc     Ping service
+// @route    GET /api/
+// @access   Public
+export default async function ping(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    exec('./scripts/hello', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        return res.status(500).json({ message: 'Failed to execute C program.' })
+      }
+      res.json({ message: stdout })
+    })
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : 'Something went wrong'
+    res.status(500).json({ message: errorMessage })
+  }
+}
---
+++ pages/api/index.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  message: string
+}
+
+// @desc     Ping service
+// @route    GET /api/
+// @access   Public
+export default async function ping(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    res.status(200).json({ message: 'Hello from Jagoncal API.' })
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : 'Something went wrong'
+    res.status(500).json({ message: errorMessage })
+  }
+}
